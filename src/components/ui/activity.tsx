dac2ba7108f312ad4fd4b99eab6a872b276caefa
akareagwom <103905242+akareagwom@users.noreply.github.com>
2023-05-10 20:20:11 +0100

    activity card _start

---
+++ src/components/ui/activity.tsx
@@ -0,0 +1,16 @@
+import {FC} from 'react';
+import { Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
+
+interface ActivityProps {
+    
+}
+ 
+const Activity: FC<ActivityProps> = () => {
+    return ( 
+        <>
+        
+        </>
+     );
+}
+ 
+export default Activity;
